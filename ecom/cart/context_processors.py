55a31435899a4fd117fc6158198b374c084421cd
Roejon Hernandez <144080078+Takamichi23@users.noreply.github.com>
2025-12-03 11:56:36 +0800

    Delete ecom directory

--- ecom/cart/context_processors.py
+++
@@ -1,6 +0,0 @@
-from .cart import Cart
-
-# Create context processor so our cart can work on all page
-def cart(request):
-    # Return the default data from pur cart
-    return {'cart':Cart(request)}
